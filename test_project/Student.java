ab706f15527b9b9996e833f9a0aa2b126e793d47
vignesh mvgs <vigneshbalanmvgs2003@gmail.com>
2024-05-18 21:00:33 +0530

    changes

---
+++ test_project/Student.java
@@ -0,0 +1,12 @@
+package test_project;
+
+public class Student {
+	String name = "bob";
+	int marks = 84;
+	
+	public void show_details(){
+		System.out.println("Name of the student  is " + name );
+
+		System.out.println("Marks scored  by the student are " + marks );
+	}
+}
---
+++ test_project/TestProject.java
@@ -0,0 +1,9 @@
+package test_project;
+
+public class TestProject{
+	public static void main(String[] agrs ){
+		Student obj = new Student();
+
+		obj.show_details();
+	}
+}
